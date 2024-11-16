fe356db6ecbace99ddf0f5724a0e307d4968e4e6
OArthr <130790176+OArthr@users.noreply.github.com>
2024-11-16 15:56:47 -0300

    Add files via upload

---
+++ Consulta.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class Consulta {
+	private Paciente paciente;
+	private Consultorio consultorio;
+	private String dataHora;
+	
+	public Consulta(Paciente paciente, Consultorio consultorio, String dataHora) {
+		this.paciente = paciente;
+		this.consultorio = consultorio;
+		this.dataHora = dataHora;
+	}
+	public String toString(Usuario u) {
+		return "- Consulta com "+ ((u instanceof Doutor)? paciente.getNome():consultorio.getDoutorResponsavel().getNome())
+				+ " no consultório " + consultorio.getNome()
+				+ " em " + dataHora;
+	}
+}
